72d7e1b495b4e677dd750b04ea246084937d74c3
Xalicus <Xalicus@fullsail.edu>
2012-08-07 18:37:56 -0700

    Committing a lot of changes to both apps.

--- Week2/Gold/js/main.js
+++ Week2/Gold/js/main.js
@@ -63,14 +63,12 @@ window.addEventListener("DOMContentLoaded", function(){
 		switch(n) {
 			case "on":
 				gebi("petForm").style.display = "none";
-				gebi("petsForm").style.display = "none";
 				gebi("clearData").style.display = "inline";
 				gebi("showData").style.display = "none";
 				gebi("addNew").style.display = "inline";
 				break;
 			case "off":
 				gebi("petForm").style.display = "block";
-				gebi("petsForm").style.display = "block";
 				gebi("clearData").style.display = "inline";
 				gebi("showData").style.display = "inline";
 				gebi("addNew").style.display = "none";
