62379d0787acabaa8ecbb4b6171109a460115494
Xalicus <Xalicus@fullsail.edu>
2012-08-10 14:53:46 -0700

    Edit my search heavily, now it works in gold. Sort of.

--- Week2/Gold/js/main.js
+++ Week2/Gold/js/main.js
@@ -319,12 +319,32 @@ window.addEventListener("DOMContentLoaded", function(){
 	};
 	
 	// My Search Function
-	function searchPet() {
-		var sp = document.index;
-		var submitTo = sp.searchengine.value + escape(sp.searchKoolPets.value);
-		window.location.href = submitTo;
-		return false;
-	};
+	// Live Text Search from "http://www.designchemical.com/blog/index.php/jquery/live-text-search-function-using-jquery/"
+	$(document).ready(function(){
+		$("#filter").keyup(function(){
+	 
+			// Retrieve the input field text and reset the count to zero
+			var filter = $(this).val(), count = 0;
+	 
+			// Loop through the KoolPets list
+			$(".itemlist li").each(function(){
+	 
+				// If the list item does not contain the text phrase fade it out
+				if ($(this).text().search(new RegExp(filter, "i")) < 0) {
+					$(this).fadeOut();
+	 
+				// Show the list item if the phrase matches and increase the count by 1
+				} else {
+					$(this).show();
+					count++;
+				}
+			});
+	 
+			// Update the count
+			var numberItems = count;
+			$("#filter-count").text("Number of KoolPets = "+count);
+		});
+	});
 	
 	makeCats();
 
